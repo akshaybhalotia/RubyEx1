7fd060c286b3f0a9ff1e8ac3d2775daad68e9a92
Akshay Bhalotia <akshay.b@multunus.com>
2015-10-30 15:58:41 +0530

    Refactoring for CRC

--- ResumeGenerator.rb
+++ ResumeGenerator.rb
@@ -1,8 +1,10 @@
 require_relative 'UserInterface'
 
+fields = [:Name, :Age, :Education, :"College/University", :"Years of experience", :"Last Company"]
+
 resume_generator = UserInterface.new
 resume_generator.display_intro
-resume_generator.read_data
+resume_generator.read_data(fields)
 resume_generator.show_formats
 resume_generator.get_choice
 resume_generator.write_to_file
--- UserInterface.rb
+++ UserInterface.rb
@@ -10,16 +10,15 @@ Hi, welcome to the Resume Generator.
 Please enter the information asked and we will generate a resume for you in the desired format.
 Thank you.
 INTRO
-    @fields = [:Name, :Age, :Education, :"College/University", :"Years of experience", :"Last Company"]
-    @info = Hash.new
   end
 
   def display_intro
     puts @intro_string
   end
 
-  def read_data
-    @fields.each do |key|
+  def read_data(fields)
+    @info = Hash.new
+    fields.each do |key|
       print key.to_s, ": "
       @info[key] = gets
     end
